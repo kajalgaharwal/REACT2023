a44bf73218ca78393e97a7910073e364c75b5102
kajal gaharwal <kajal.gaharwal@gmail.com>
2024-01-09 21:58:53 +0530

    react router dom

---
+++ reactRouter/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+import {useParams} from 'react-router-dom';
+function User() {
+    const {userid} = useParams();
+  return (
+    <div>
+      user:{userid}
+    </div>
+  )
+}
+
+export default User
