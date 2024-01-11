df42e52a2b1a45f67fa259c7c796364477bc72a2
kajal gaharwal <kajal.gaharwal@gmail.com>
2024-01-11 17:15:10 +0530

    todo with redux toolkit

---
+++ reduxTodoApp/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodos />
+      <Todos />
+    </>
+  )
+}
+
+export default App
